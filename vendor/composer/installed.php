88c2180e8d801f2193e6c7c1ef23275bf3ff1ec0
YJSoft <yjsoft@yjsoft.xyz>
2025-03-20 04:47:29 +0000

    vendor 커밋

---
+++ vendor/composer/installed.php
@@ -0,0 +1,187 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yjsoft/rx-to-sns',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0e42c878f4ff853f569b0215576a72d40f677dd7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abraham/twitteroauth' => array(
+            'pretty_version' => '7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'a8622680f954d0c4f54fd80bedebe00c756ed7aa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../abraham/twitteroauth',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bordoni/phpass' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '0.3.6',
+            ),
+        ),
+        'composer/ca-bundle' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'coolsms/php-sdk' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.*',
+            ),
+        ),
+        'doctrine/lexer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.*',
+            ),
+        ),
+        'egulias/email-validator' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '3.2.*',
+            ),
+        ),
+        'enshrined/svg-sanitize' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '0.*',
+            ),
+        ),
+        'ezyang/htmlpurifier' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '4.18.*',
+            ),
+        ),
+        'firebase/php-jwt' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '6.4.0',
+            ),
+        ),
+        'google/auth' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.26.*',
+            ),
+        ),
+        'guzzlehttp/guzzle' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '7.9.*',
+            ),
+        ),
+        'guzzlehttp/psr7' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.7.*',
+            ),
+        ),
+        'jbbcode/jbbcode' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.4.*',
+            ),
+        ),
+        'leafo/lessphp' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'dev-master',
+            ),
+        ),
+        'league/html-to-markdown' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '5.1.*',
+            ),
+        ),
+        'matthiasmullie/minify' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.3.*',
+            ),
+        ),
+        'matthiasmullie/path-converter' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.1.*',
+            ),
+        ),
+        'michelf/php-markdown' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.9.*',
+            ),
+        ),
+        'michelf/php-smartypants' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.8.*',
+            ),
+        ),
+        'psr/cache' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.0.1',
+            ),
+        ),
+        'rhymix/composer-stub' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c7d4fcec98e99d9e5213c5fc96d35d406e6d0848',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rhymix/composer-stub',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'rmccue/requests' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.8.*',
+            ),
+        ),
+        'scssphp/scssphp' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '1.13.*',
+            ),
+        ),
+        'swiftmailer/swiftmailer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '6.3.0',
+            ),
+        ),
+        'symfony/deprecation-contracts' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.5.*',
+            ),
+        ),
+        'true/punycode' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '2.*',
+            ),
+        ),
+        'yjsoft/rx-to-sns' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0e42c878f4ff853f569b0215576a72d40f677dd7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
